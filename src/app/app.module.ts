f6a2e2582a426ca0df4b1c3f892a74577a722dad
kiranpuppala <kiranpuppala.96@gmail.com>
2019-03-22 19:37:28 +0530

    minor improvments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    UserDashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
